2be85dea6691d920db075077d797cf53fe5cfe59
Max <135263966+verifizieren@users.noreply.github.com>
2024-05-29 12:24:19 -0700

    Update menu.h
    Cleaned up code

--- menu.h
+++ menu.h
@@ -1,182 +1,107 @@
 #pragma once
-#include "pch.h"
+#include <string>
+#include <vector>
+#include <variant>
+#include <iostream>
+#include <windows.h>
 
-void Box(int color) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-	std::cout << "[";
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-	std::cout << "*";
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-	std::cout << "] ";
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-}
+class Menu2 {
+private:
+    std::vector<std::pair<std::string, std::variant<int*, float*, bool*, std::string*, DWORD*, HANDLE*, std::string>>> features;
+    std::string gameName;
+    std::string architecture;
+    std::string version;
+    DWORD procId;
+    std::string closeKey;
 
-namespace proc {
-	DWORD GetProcId(const wchar_t* procName)
-	{
-		DWORD procId = 0;
-		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-		if (hSnap != INVALID_HANDLE_VALUE)
-		{
-			PROCESSENTRY32 procEntry;
-			procEntry.dwSize = sizeof(procEntry);
+    void Box(int color) {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+        std::cout << "[";
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+        std::cout << "*";
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+        std::cout << "] ";
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    }
 
-			if (Process32First(hSnap, &procEntry))
-			{
-				do
-				{
-					if (!_wcsicmp(procEntry.szExeFile, procName))
-					{
-						procId = procEntry.th32ProcessID;
-						break;
-					}
-				} while (Process32Next(hSnap, &procEntry));
+    void DisplayFeature(const std::string& featureName, const std::variant<int*, float*, bool*, std::string*, DWORD*, HANDLE*, std::string>& featureValue) {
+        if (std::holds_alternative<std::string>(featureValue)) {
+            Box(3);
+            std::cout << std::get<std::string>(featureValue) << std::endl;
+        }
+        else {
+            Box(3);
+            std::cout << featureName << ": ";
 
-			}
-		}
-		CloseHandle(hSnap);
-		return procId;
-	}
+            if (std::holds_alternative<bool*>(featureValue)) {
+                int color = *std::get<bool*>(featureValue) ? 10 : 4;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+                std::cout << (*std::get<bool*>(featureValue) ? "Enabled" : "Disabled");
+            }
+            else if (std::holds_alternative<int*>(featureValue) || std::holds_alternative<float*>(featureValue)) {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+                if (std::holds_alternative<int*>(featureValue)) {
+                    std::cout << *std::get<int*>(featureValue);
+                }
+                else {
+                    std::cout << *std::get<float*>(featureValue);
+                }
+            }
+            else if (std::holds_alternative<std::string*>(featureValue)) {
+                std::cout << *std::get<std::string*>(featureValue);
+            }
+            else if (std::holds_alternative<DWORD*>(featureValue)) {
+                std::cout << *std::get<DWORD*>(featureValue);
+            }
+            else if (std::holds_alternative<HANDLE*>(featureValue)) {
+                std::cout << *std::get<HANDLE*>(featureValue);
+            }
 
-	uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
-	{
-		uintptr_t modBaseAddr = 0;
-		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
-		if (hSnap != INVALID_HANDLE_VALUE)
-		{
-			MODULEENTRY32 modEntry;
-			modEntry.dwSize = sizeof(modEntry);
-			if (Module32First(hSnap, &modEntry))
-			{
-				do
-				{
-					if (!_wcsicmp(modEntry.szModule, modName))
-					{
-						modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
-						break;
-					}
-				} while (Module32Next(hSnap, &modEntry));
-			}
-		}
-		CloseHandle(hSnap);
-		return modBaseAddr;
-	}
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+            std::cout << std::endl;
+        }
+    }
 
-	uintptr_t FindDMAAaddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
-	{
-		uintptr_t addr = ptr;
-		for (unsigned int i = 0; i < offsets.size(); i++)
-		{
-			ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
-			addr += offsets[i];
-		}
-		return addr;
-	}
-}
-
-namespace mem {
-	void PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess)
-	{
-		DWORD oldprotect;
-		VirtualProtectEx(hProcess, dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
-		WriteProcessMemory(hProcess, dst, src, size, nullptr);
-		VirtualProtectEx(hProcess, dst, size, oldprotect, &oldprotect);
-	}
-
-	void NopEx(BYTE* dst, unsigned int size, HANDLE hProcess)
-	{
-		BYTE* nopArray = new BYTE[size];
-		memset(nopArray, 0x90, size);
-
-		PatchEx(dst, nopArray, size, hProcess);
-		delete[] nopArray;
-	}
-
-	uintptr_t FindDMAAaddyEx(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
-	{
-		uintptr_t addr = ptr;
-		for (unsigned int i = 0; i < offsets.size(); i++)
-		{
-			ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
-			addr += offsets[i];
-		}
-		return addr;
-	}
-
-	void Patch(BYTE* dst, BYTE* src, unsigned int size)
-	{
-		DWORD oldprotect;
-		VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
-		memcpy(dst, src, size);
-		VirtualProtect(dst, size, oldprotect, &oldprotect);
-	}
-
-	void Nop(BYTE* dst, unsigned int size)
-	{
-		DWORD oldprotect;
-		VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
-		memset(dst, 0x90, size);
-		VirtualProtect(dst, size, oldprotect, &oldprotect);
-	}
-
-	uintptr_t FindDMAAaddy(uintptr_t ptr, std::vector<unsigned int> offsets)
-	{
-		uintptr_t addr = ptr;
-		for (unsigned int i = 0; i < offsets.size(); ++i)
-		{
-			addr = *(uintptr_t*)addr;
-			addr += offsets[i];
-		}
-		return addr;
-	}
-}
-
-class Menu {
 public:
-	void Header(const char* header, const char* arch, const char* ver) {
-		std::cout << "| Game: " << header << std::endl;
-		std::cout << "| Architecture: " << arch << std::endl;
-		std::cout << "| Version: " << ver << std::endl << std::endl;
-	}
+    void Header(const std::string& header, const std::string& arch, const std::string& ver, DWORD pid) {
+        gameName = header;
+        architecture = arch;
+        version = ver;
+        procId = pid;
+    }
 
-	void BoolFeat(const char* feature, bool status) {
-		const char* text = "";
-		int color = 4;
+    void AddFeature(const std::string& featureName, std::variant<int*, float*, bool*, std::string*, DWORD*, HANDLE*, std::string> featureValue) {
+        features.push_back({ featureName, featureValue });
+    }
 
-		switch (status) {
-		case true:
-			text = "Enabled";
-			color = 10;
-			break;
-		case false:
-			text = "Disabled";
-			color = 4;
-			break;
-		}
-		Box(3);
-		std::cout << feature << ": ";
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-		std::cout << text;
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-		std::cout << std::endl;
-	}
+    void UpdateFeature(const std::string& featureName, std::variant<int*, float*, bool*, std::string*, DWORD*, HANDLE*, std::string> featureValue) {
+        for (auto& feature : features) {
+            if (feature.first == featureName) {
+                feature.second = featureValue;
+                break;
+            }
+        }
+    }
 
-	void IntFeat(const char* feature, int status) {
-		Box(3);
-		std::cout << feature << ": ";
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
-		std::cout << status;
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-		std::cout << std::endl;
-	}
+    void Display() {
+        system("cls");
+        std::cout << "| Game: " << gameName << std::endl;
+        std::cout << "| Architecture: " << architecture << std::endl;
+        std::cout << "| Version: " << version << std::endl;
+        std::cout << "| Process ID: " << procId << std::endl << std::endl;
+        for (const auto& feature : features) {
+            DisplayFeature(feature.first, feature.second);
+        }
+        std::cout << std::endl;
+        Box(3);
+        std::cout << "Press " << closeKey << " to exit" << std::endl;
+    }
 
-	void FloatFeat(const char* feature, float status) {
-		Box(3);
-		std::cout << feature << ": ";
+    void TextItem(const std::string& text) {
+        features.push_back({ text, text });
+    }
 
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
-		std::cout << status;
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-		std::cout << std::endl;
-	}
+    void setCloseKey(const std::string& key) {
+        closeKey = key;
+    }
 };
